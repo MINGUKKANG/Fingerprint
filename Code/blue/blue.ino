d70f15fd4a4296f00fb936c2e90187b175985a36
MingukKANG <30516904+MINGUKKANG@users.noreply.github.com>
2018-05-27 18:24:50 +0900

    Add files via upload

---
+++ Code/blue/blue.ino
@@ -0,0 +1,18 @@
+#include "SoftwareSerial.h"
+SoftwareSerial BTSerial(2,3);
+char val;
+
+void setup() {
+  Serial.begin(9600);
+  BTSerial.begin(9600);
+}
+
+void loop() {   
+  if (BTSerial.available()){
+    val = BTSerial.read();
+    Serial.write(val);
+    delay(500);
+    Serial.print(val);
+  }
+}
+
