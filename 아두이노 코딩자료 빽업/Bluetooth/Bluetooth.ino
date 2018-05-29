013190aba764308d33de71491ecbd28d31115bd7
MingukKANG <30516904+MINGUKKANG@users.noreply.github.com>
2018-05-29 09:49:47 +0900

    Add files via upload

---
+++ 아두이노 코딩자료 빽업/Bluetooth/Bluetooth.ino
@@ -0,0 +1,16 @@
+#include "SoftwareSerial.h"
+SoftwareSerial BTSerial(2,3);
+int val;
+
+void setup() {
+  Serial.begin(115200);
+  BTSerial.begin(9600);
+}
+
+void loop() {
+  if(BTSerial.available()){
+    val = int(BTSerial.read());
+    if(val == 49){
+      Serial.write(0);
+    }}
+}
---
+++ 아두이노 코딩자료 빽업/Recognition/Recognition.ino
@@ -0,0 +1,72 @@
+/* 
+  FPS_Enroll.ino - Library example for controlling the GT-511C3 Finger Print Scanner (FPS)
+  Created by Josh Hawley, July 23rd 2013
+  Licensed for non-commercial use, must include this license message
+  basically, Feel free to hack away at it, but just give me credit for my work =)
+  TLDR; Wil Wheaton's Law
+  This sketch will attempt to identify a previously enrolled fingerprint.
+*/
+#include "FPS_GT511C3.h"
+#include "SoftwareSerial.h"
+// Hardware setup - FPS connected to:
+// digital pin 2(arduino rx, fps tx)
+// digital pin 3(arduino tx - 560ohm resistor fps tx - 1000ohm resistor - ground)3
+// this brings the 5v tx line down to about 3.2v so we dont fry our fps
+
+FPS_GT511C3 fps(2, 3);
+int val = 3;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(4, OUTPUT);
+  pinMode(6, OUTPUT);
+  fps.Open();
+  fps.SetLED(true);
+}
+
+void loop() { 
+  if(Serial.available()){
+    int val = Serial.read();
+    Serial.print(val);
+    if(val == 0){
+      tone(6,261);
+      delay(500); 
+      tone(6,294);
+      delay(500);
+      tone(6,330);
+      delay(500);
+      noTone(6);
+      digitalWrite(4,HIGH);
+      delay(1500);
+      digitalWrite(4,LOW);
+      delay(1500);
+      Serial.end();
+      Serial.begin(115200);   
+    }
+    
+
+  // Identify fingerprint test
+  if (fps.IsPressFinger()) {
+    fps.CaptureFinger(false);
+    int id = fps.Identify1_N();
+    if (id <200) {     
+      tone(6,261);
+      delay(500);
+      tone(6,294);
+      delay(500);
+      tone(6,330);
+      delay(500);
+      noTone(6);
+      
+      digitalWrite(4,HIGH);
+      delay(1500);
+      digitalWrite(4,LOW);
+      delay(1500);
+      }else {
+      tone(6,349);
+      delay(500);
+      tone(6,349);
+      delay(500);
+      noTone(6);}
+  }
+  }}
